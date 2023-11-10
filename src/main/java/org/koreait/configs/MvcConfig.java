be5d114980741f12e7a834fc4fbc42f2969bdf3b
yonggyo1125 <yonggyo00@kakao.com>
2023-11-10 17:12:34 +0900

    설정 중...

--- src/main/java/org/koreait/configs/MvcConfig.java
+++ src/main/java/org/koreait/configs/MvcConfig.java
@@ -2,8 +2,10 @@ package org.koreait.configs;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -24,6 +26,17 @@ public class MvcConfig implements WebMvcConfigurer {
 
     @Bean
     public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
+
         return new HiddenHttpMethodFilter();
     }
+
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource ms = new ResourceBundleMessageSource();
+        ms.setDefaultEncoding("UTF-8");
+        ms.addBasenames("messages.commons", "messages.validations", "messages.errors");
+
+        return ms;
+    }
+
 }

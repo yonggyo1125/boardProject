72596a251f725ed90c08cb86ca41ce06b249dcd8
yonggyo1125 <yonggyo00@kakao.com>
2023-11-10 14:17:46 +0900

    설정 중...

---
+++ src/main/java/org/koreait/configs/MvcConfig.java
@@ -0,0 +1,20 @@
+package org.koreait.configs;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableConfigurationProperties(FileUploadConfig.class)
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private FileUploadConfig fileUploadConfig;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+            System.out.println("fileUploadConfig : " + fileUploadConfig);
+    }
+}

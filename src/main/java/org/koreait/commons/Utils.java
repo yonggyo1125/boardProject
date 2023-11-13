4305b9422e08008b2ecf41e549cf0e4bb42c6894
yonggyo1125 <yonggyo00@kakao.com>
2023-11-13 14:19:16 +0900

    모바일, PC 분리

--- src/main/java/org/koreait/commons/Utils.java
+++ src/main/java/org/koreait/commons/Utils.java
@@ -1,14 +1,19 @@
 package org.koreait.commons;
 
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.util.Objects;
 import java.util.ResourceBundle;
 
 @Component
+@RequiredArgsConstructor
 public class Utils {
     private static ResourceBundle validationsBundle;
     private static ResourceBundle errorsBundle;
+
+    private final HttpServletRequest request;
 
     static {
         validationsBundle = ResourceBundle.getBundle("messages.validations");
@@ -23,5 +28,12 @@ public class Utils {
         } catch (Exception e) {
             return null;
         }
+    }
+
+    public boolean isMobile() {
+        // 요청 헤더 User-Agent
+        boolean isMobile = request.getHeader("User-Agent").matches(".*(iPhone|iPod|iPad|BlackBerry|Android|Windows CE|LG|MOT|SAMSUNG|SonyEricsson).*");
+
+        return isMobile;
     }
 }

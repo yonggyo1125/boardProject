58fb424ddeb0d42d16dfbd5ea3a6b046abc78804
yonggyo1125 <yonggyo00@kakao.com>
2023-11-16 17:01:22 +0900

    설정 수정

--- src/test/java/org/koreait/jpaex/Ex04.java
+++ src/test/java/org/koreait/jpaex/Ex04.java
@@ -6,10 +6,17 @@ import org.koreait.entities.BoardData;
 import org.koreait.repositories.BoardDataRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.context.TestPropertySource;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static org.springframework.data.domain.Sort.Order.asc;
+import static org.springframework.data.domain.Sort.Order.desc;
 
 @SpringBootTest
 @TestPropertySource(properties = "spring.profiles.active=test")
@@ -34,7 +41,19 @@ public class Ex04 {
 
     @Test
     void test1() {
+        List<BoardData> items = repository.findAll(Sort.by(desc("seq"), asc("subject")));
 
+        items.stream().forEach(System.out::println);
+
+    }
+
+    @Test
+    void test2() {
+        Pageable pageable = PageRequest.of(0, 3);
+
+        Page<BoardData> data = repository.findAll(pageable);
+        List<BoardData> items = data.getContent();
+        items.stream().forEach(System.out::println);
     }
 
 }

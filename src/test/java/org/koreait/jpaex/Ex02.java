766885061e6eeaf4915584b85b837c2a628aaf7d
yonggyo1125 <yonggyo00@kakao.com>
2023-11-16 09:23:34 +0900

    설정 수정

--- src/test/java/org/koreait/jpaex/Ex02.java
+++ src/test/java/org/koreait/jpaex/Ex02.java
@@ -6,10 +6,12 @@ import org.junit.jupiter.api.Test;
 import org.koreait.commons.constants.MemberType;
 import org.koreait.entities.Member;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 @Transactional
+@TestPropertySource(properties = "spring.profiles.active=test")
 public class Ex02 {
 
     @PersistenceContext

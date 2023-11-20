0e8b62f78542ee7e7d5484a29df396aa4b5ab52d
yonggyo1125 <yonggyo00@kakao.com>
2023-11-20 16:21:37 +0900

    설정 변경

--- src/main/java/org/koreait/entities/Member.java
+++ src/main/java/org/koreait/entities/Member.java
@@ -1,27 +1,33 @@
 package org.koreait.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.koreait.commons.constants.MemberType;
-import org.springframework.data.annotation.Id;
 
-import java.time.LocalDateTime;
-
-@Data
+@Entity
+@Data @Builder
+@NoArgsConstructor @AllArgsConstructor
 public class Member {
 
-    @Id
+    @Id @GeneratedValue
     private Long userNo;
 
+    @Column(length=65, unique = true, nullable = false)
     private String email;
 
+    @Column(length=65, nullable = false)
     private String password;
 
+    @Column(length=40, nullable=false)
     private String userNm;
 
+    @Column(length=11)
     private String mobile;
 
+    @Enumerated(EnumType.STRING)
+    @Column(length=15, nullable = false)
     private MemberType mtype = MemberType.USER;
-
-    private LocalDateTime regDt;
-    private LocalDateTime modDt;
 }

f4addfe4b606b5491be917a61354a2f70bd60408
yonggyo1125 <yonggyo00@kakao.com>
2023-11-21 14:53:13 +0900

    설정 변경

--- src/main/java/org/koreait/commons/MemberUtil.java
+++ src/main/java/org/koreait/commons/MemberUtil.java
@@ -2,6 +2,7 @@ package org.koreait.commons;
 
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import org.koreait.commons.constants.MemberType;
 import org.koreait.entities.Member;
 import org.springframework.stereotype.Component;
 
@@ -12,6 +13,14 @@ public class MemberUtil {
 
     public boolean isLogin() {
         return getMember() != null;
+    }
+
+    /**
+     * 관리자 여부 체크
+     * @return
+     */
+    public boolean isAdmin() {
+        return isLogin() && getMember().getMtype() == MemberType.ADMIN;
     }
 
     public Member getMember() {

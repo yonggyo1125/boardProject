00bf224ef669a382a1ba35cbdc672e2d154e1273
yonggyo1125 <yonggyo00@kakao.com>
2023-11-22 15:17:03 +0900

    설정 변경

--- src/main/java/org/koreait/entities/Base.java
+++ src/main/java/org/koreait/entities/Base.java
@@ -1,5 +1,6 @@
 package org.koreait.entities;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
@@ -16,6 +17,7 @@ import java.time.LocalDateTime;
 public abstract class Base {
     @CreatedDate
     @Column(updatable = false)
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
     private LocalDateTime createdAt;
 
     @LastModifiedDate

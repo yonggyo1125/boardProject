57c1774338657e58aa99c6606ebf0264c3b5ace7
yonggyo1125 <yonggyo00@kakao.com>
2023-11-22 16:54:21 +0900

    설정 변경

--- src/main/java/org/koreait/configs/MvcConfig.java
+++ src/main/java/org/koreait/configs/MvcConfig.java
@@ -1,6 +1,7 @@
 package org.koreait.configs;
 
 import org.koreait.commons.interceptors.CommonInterceptor;
+import org.koreait.commons.interceptors.SiteConfigInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.MessageSource;
@@ -26,10 +27,15 @@ public class MvcConfig implements WebMvcConfigurer {
     @Autowired
     private CommonInterceptor commonInterceptor;
 
+    @Autowired
+    private SiteConfigInterceptor siteConfigInterceptor;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(commonInterceptor)
+                .addPathPatterns("/**");
+
+        registry.addInterceptor(siteConfigInterceptor)
                 .addPathPatterns("/**");
 
     }
